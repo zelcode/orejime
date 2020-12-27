681e3c2e61612421b7595c225f00f563fcf6e552
zelcode <zeljko.debeljuh@gmail.com>
2020-12-27 14:59:59 +0100

    Translations

--- src/translations/index.js
+++ src/translations/index.js
@@ -1,22 +1,10 @@
 // To create a new translation, add a YAML file with the required entries and
 // link it here using the appropriate language code.
 
-import de from './de.yml'
 import en from './en.yml'
-import fr from './fr.yml'
-import hu from './hu.yml'
-import it from './it.yml'
-import ro from './ro.yml'
-import nb from './nb.yml'
-import nl from './nl.yml'
+import hr from './hr.yml'
 
 export default {
-  de,
   en,
-  fr,
-  hu,
-  it,
-  ro,
-  nb,
-  nl,
+  hr
 }
